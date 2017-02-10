ab0b82f10d707d0518b1b30115d7046da077736b
RMSD <RMSD@users.noreply.github.com>
2017-02-09 21:19:25 -0800

    Create main.go

---
+++ src/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"github.com/trakerr-io/trakerr-go/src/traker"
+	"github.com/trakerr-io/trakerr-go/src/traker"
+)
+
+// Build is the git sha of this binaries build.
+var Build string
+
+func main() {
+}
